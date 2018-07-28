3ad7f5cc84544525e7acc97f6a8e34d95eef406f
grjava <457013078@qq.com>
2018-07-28 16:38:21 +0800

    让坦克动起来的，使用匿名类启动测试线程。

--- TankWar.java
+++ TankWar.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 
 class TankWar extends Frame {
 
+        int x=50,y=50;
         public TankWar(){
                 this.setSize(400,300);
                 this.setVisible(true);
@@ -15,14 +16,34 @@ class TankWar extends Frame {
                 });
                 this.setResizable(false);
                 this.setTitle("guorui");
+
+                new Thread(){
+                        public void run(){
+                                while (true){
+                                        repaint();
+                                        try {
+                                                Thread.sleep(500);
+                                        } catch(Exception e) {
+                                                e.printStackTrace();
+                                        }
+                                }
+                        }
+                }.start();
+
+
         }
 
         public void paint(Graphics g){
                 Color c = g.getColor();
                 g.setColor(Color.RED);
-                g.fillOval(50,50,30,30);
+                g.fillOval(x,y,30,30);
                 g.setColor(c);
 
+        }
+
+        public void repaint(){
+                x += 5;
+                super.repaint();
         }
 
         public static void main (String [] args) {
